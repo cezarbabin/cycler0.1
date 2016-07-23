9eea87815fcb2168c1357f3cb0e81e96fd37f8dc
nibab <nibab@Cezars-MacBook-Pro.local>
2016-07-23 16:35:56 -0400

    added terrain generator

---
+++ terrain/assets/js/main.js
@@ -0,0 +1,32 @@
+var MAKETERRAIN = {
+	WithParams : function(parameters){
+		var temp = {
+			alea: RAND_MT,
+			generator: PN_GENERATOR,
+			width: 500,
+			height: 500,
+			widthSegments: 30,
+			heightSegments: 30,
+			depth: 30,
+			param: 3,
+			filterparam: 1,
+			filter: [ BLUR_FILTER ],
+			postgen: [ MOUNTAINS2_COLORS ],
+			effect: [ DESTRUCTURE_EFFECT ]
+		}
+
+		
+		//TERRAINGENDEMO.Initialize( 'canvas-3d', parameters );
+		// GUI.Initialize( parameters );
+		
+		//WINDOW.ResizeCallback = function( inWidth, inHeight ) { TERRAINGENDEMO.Resize( inWidth, inHeight ); };
+		//TERRAINGENDEMO.Resize( WINDOW.ms_Width, WINDOW.ms_Height );
+		
+
+		// Return the Mesh Here
+		var mesh = TERRAINGENDEMO.Load(temp);
+		return mesh;
+		//MainLoop();
+	}
+}
+
